37e20168573e7103a26970a61fff016746b55839
AbhayDedkawala <abhaydedje@gmail.com>
2024-03-21 21:20:01 +0530

    Add C# Console application

---
+++ Practice-DSA-CSharp/ArrayDSA.cs
@@ -0,0 +1,74 @@
+namespace DSA
+{
+    public class ArrayDSA : IDisposable
+    {
+        public ArrayDSA()
+        {
+            Console.Clear();
+        }
+
+        public void Dispose()
+        {
+            Console.Read();
+        }
+
+
+        /// <summary>
+        /// Reverse Integer Array <br/>
+        /// Input Array : { 1, 5, 8, 2, 3 } <br/>
+        /// Output Array : { 3, 2, 8, 5, 1 }
+        /// </summary>
+        public void ArrayIntegerReverse()
+        {
+            int[] numbers = new int[] { 1, 5, 0, 3, 4, 2 };
+
+            Console.WriteLine("Before array reverse.");
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(string.Format("{0} -> {1}", i, numbers[i]));
+
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Length - i - 1];
+                numbers[numbers.Length - i - 1] = numbers[i] - numbers[numbers.Length - i - 1];
+                numbers[i] = numbers[i] - numbers[numbers.Length - i - 1];
+            }
+
+            Console.WriteLine("\nAfter array reverse.");
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(string.Format("{0} -> {1}", i, numbers[i]));
+        }
+
+        /// <summary>
+        /// Sort Integer Array <br/>
+        /// Input Array : { 1, 5, 8, 2, 3 } <br/>
+        /// Output Array : { 1, 2, 3, 5, 8 }
+        /// </summary>
+        public void ArrayIntegerSort()
+        {
+            bool isAscending = false;
+            int[] numbers = new int[] { 1, 5, 0, 3, 4, 2 };
+
+            Console.WriteLine("Before array sorting.");
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(string.Format("{0} -> {1}", i, numbers[i]));
+
+            for (int i = 0; i < numbers.Length; i++)
+                for (int j = i; j < numbers.Length; j++)
+                    if ((isAscending && numbers[i] > numbers[j]) || (!isAscending && numbers[i] < numbers[j]))
+                    {
+                        numbers[i] += numbers[j];
+                        numbers[j] = numbers[i] - numbers[j];
+                        numbers[i] = numbers[i] - numbers[j];
+                    }
+
+
+            Console.WriteLine("\nAfter array sorting.");
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(string.Format("{0} -> {1}", i, numbers[i]));
+        }
+    }
+}
---
+++ Practice-DSA-CSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace DSA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isRun = true;
+            while (isRun)
+            {
+                Console.Clear();
+
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine("Select Options : ");
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Array Sort");
+                Console.WriteLine("2. Array Reverse");
+                Console.WriteLine("=====================================================================");
+
+                string? program = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(program))
+                    ProgramPerform(program);
+            }
+        }
+
+        public static void ProgramPerform(string program)
+        {
+            switch (program)
+            {
+                case "1":
+                    using (ArrayDSA array = new ArrayDSA())
+                        array.ArrayIntegerSort();
+
+                    break;
+
+                case "2":
+                    using (ArrayDSA array = new ArrayDSA())
+                        array.ArrayIntegerReverse();
+
+                    break;
+
+                case "0":
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
